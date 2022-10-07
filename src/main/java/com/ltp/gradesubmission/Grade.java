ee7821c8e0d6b0a6016a76f4d45d25debdb70059
Gokhan Yigit <yigitgokhan1@gmail.com>
2022-10-07 17:36:55 +0200

    Add basic Validation

--- src/main/java/com/ltp/gradesubmission/Grade.java
+++ src/main/java/com/ltp/gradesubmission/Grade.java
@@ -2,15 +2,24 @@ package com.ltp.gradesubmission;
 
 import java.util.UUID;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Grade {
+
+   
     private String id;
+
+    @NotBlank(message= "Name cannot be blank")
     private String name;
+    @NotBlank(message= "Subject cannot be blank")
     private String subject;
+    @Max(value= 100, message = "Score can not be more than 100")
     private String score;
 
     public Grade(){
