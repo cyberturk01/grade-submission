c1e9987203bed51692425847e1b8af271433dfd4
Gokhan Yigit <yigitgokhan1@gmail.com>
2022-10-10 10:34:02 +0200

    Add Custom Constraint Score

--- src/main/java/com/ltp/gradesubmission/Grade.java
+++ src/main/java/com/ltp/gradesubmission/Grade.java
@@ -1,10 +1,7 @@
 package com.ltp.gradesubmission;
 
 import java.util.UUID;
-
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,14 +9,16 @@ import lombok.Data;
 @AllArgsConstructor
 public class Grade {
 
-   
     private String id;
 
     @NotBlank(message= "Name cannot be blank")
     private String name;
+
     @NotBlank(message= "Subject cannot be blank")
     private String subject;
-    @Max(value= 100, message = "Score can not be more than 100")
+
+    //Create Custom Constraint
+   @Score(message = "Score must be a letter grade")
     private String score;
 
     public Grade(){
